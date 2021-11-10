df27ffb86f73804ff9f8af3c9667cf40eba3e30b
karun100 <karun@karuns-MacBook-Pro.local>
2021-11-10 07:34:48 +0530

    third_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import { withStyles } from '@material-ui/core/styles';
+import { ExpandMore } from '@material-ui/icons';
+
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+
+import { AccordionActions, Button, Divider } from '@material-ui/core';
+
+const Accordion = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    borderRadius: '30px 0px 30px 0',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: '#21CFFF',
+    borderBottom: '1px solid #12738E',
+    marginBottom: -1,
+    color: '#666666',
+    borderRadius: '30px 0px 30px 0',
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0',
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Accordion>
+
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          Accordion 1
+        </AccordionSummary>
+
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+          sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+
+        <Divider />
+        <AccordionActions>
+          <Button size="small" onClick={() => alert('cancel')}>Cancel</Button>
+          <Button size="small" color="primary" onClick={() => alert('save')}>Save</Button>
+        </AccordionActions>
+
+      </Accordion>
+
+      <Accordion>
+
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          Accordion 2
+        </AccordionSummary>
+
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+          sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+
+      </Accordion>
+
+      <Accordion>
+
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          Restart AOS
+        </AccordionSummary>
+
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+          sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+
+      </Accordion>
+
     </div>
   );
 }
